021d3b04a30ff23a654e53ddba083dbbf0d16bba
Sithija Siriwardana <sithijalawan@gmail.com>
2019-10-05 14:43:04 +0530

    program to reverse the numbers inserted in a stack using basic stack operations

---
+++ Stacks/program to reverse the numbers inserted in a stack using basic stack operations.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<string.h>
+ 
+#define MAX 20
+ 
+int element;
+int top = -1;
+char stack_array[MAX];
+void push(char element);
+int isEmpty();
+int isFull();
+char pop();
+ 
+void main()
+{
+        char stack_string[30];
+        int count;
+        printf("Enter A String:\t");
+        for(count = 0; count < strlen(stack_string); count++){
+        scanf("%d", &stack_string[count]);
+    }
+        for(count = 0; count < strlen(stack_string); count++)
+        {
+                push(stack_string[count]);
+        }
+        for(count = 0; count < strlen(stack_string); count++)
+        {
+                stack_string[count] = pop();
+        }
+        for(count = 0; count < strlen(stack_string); count++){
+        	printf("%d", &stack_string[count]);
+        printf("\n");
+		}
+        
+}
+ 
+void push(char element)
+{
+        if(isFull())
+        {
+                printf("\nStack Overflow\n");
+        }
+        else
+        {
+                top = top + 1;
+                stack_array[top] = element;
+        }
+}
+ 
+char pop()
+{
+        if(isEmpty())
+        {
+                printf("\nStack Underflow\n");
+                return 1;
+        }
+        else
+        {
+                element = stack_array[top];
+                top = top - 1;
+                return element;
+        }
+}
+ 
+int isEmpty()
+{
+        if(top == -1)
+                return 1;
+        else
+                return 0;
+}
+ 
+int isFull()
+{
+        if(top == MAX-1)
+                return 1;
+        else
+                return 0;
+}
